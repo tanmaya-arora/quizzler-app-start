074b8b9303f12c41fbe2ce8a543a5ff1d17a35a2
tanmaya-arora <tanmaya.arora@ikokas.com>
2024-02-19 12:42:08 +0530

    Right and wrong buttons made functional

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,7 +4,7 @@ import html
 class QuizBrain:
 
     def __init__(self, q_list):
-        self.question_number = 0
+        self.question_number = -1
         self.score = 0
         self.question_list = q_list
         self.current_question = None
@@ -16,10 +16,12 @@ class QuizBrain:
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
-        user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        self.check_answer(user_answer)
+        return f"Q.{self.question_number+1}: {q_text}"
+        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
+        # self.check_answer(user_answer)
 
     def check_answer(self, user_answer):
+        self.current_question = self.question_list[self.question_number]
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
@@ -27,5 +29,5 @@ class QuizBrain:
         else:
             print("That's wrong.")
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
+        print(f"Your current score is: {self.score}/{self.question_number+1}")
         print("\n")
--- ui.py
+++ ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
         self.window = Tk()
+        self.qb = quiz_brain
         self.window.title("Quizzler")
         self.window.config(pady=20, padx=20, bg=THEME_COLOR)
 
@@ -17,6 +19,9 @@ class QuizInterface:
         self.empty_label.grid(column=1, row=2)
 
         self.canvas = Canvas(width=300, height=250)
+        self.question_text = self.canvas.create_text(
+            150, 125, text="", width=280, fill=THEME_COLOR, font=("Arial", 20, "italic")
+        )
         self.canvas.grid(column=1, row=3, columnspan=2)
 
         self.empty_label2 = Label(text="", bg=THEME_COLOR)
@@ -25,10 +30,22 @@ class QuizInterface:
         self.right_img = PhotoImage(file="./images/true.png")
         self.wrong_img = PhotoImage(file="./images/false.png")
 
-        self.right_btn = Button(image=self.right_img, bg=THEME_COLOR, highlightthickness=0)
+        self.right_btn = Button(
+            image=self.right_img, bg=THEME_COLOR, highlightthickness=0, command=self.true_pressed)
         self.right_btn.grid(column=1, row=5)
 
-        self.wrong_btn = Button(image=self.wrong_img, bg=THEME_COLOR, highlightthickness=0)
+        self.wrong_btn = Button(
+            image=self.wrong_img, bg=THEME_COLOR, highlightthickness=0, command=self.false_pressed)
         self.wrong_btn.grid(column=2, row=5)
 
+        self.canvas.itemconfig(self.question_text, text=quiz_brain.next_question())
+
         self.window.mainloop()
+
+    def true_pressed(self):
+        self.qb.check_answer("true")
+        self.canvas.itemconfig(self.question_text, text=self.qb.next_question())
+
+    def false_pressed(self):
+        self.qb.check_answer("false")
+        self.canvas.itemconfig(self.question_text, text=self.qb.next_question())
